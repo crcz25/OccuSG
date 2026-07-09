d278c4ba9bbeda986ad4f2336544c67ef27e6793
Carlos Cueto <crcueto25@gmail.com>
2026-07-09 16:03:16 +0300

    update download script
    add smaller clip version

--- scripts/download_semantic_perception_models.sh
+++ scripts/download_semantic_perception_models.sh
@@ -9,8 +9,9 @@ models_dir="$workspace_root/models"
 groundingdino_dir="$models_dir/groundingdino"
 mobilesam_dir="$models_dir/mobilesam"
 labels_dir="$models_dir/labels"
+clip_dir="$models_dir/clip"
 
-mkdir -p "$groundingdino_dir" "$mobilesam_dir" "$labels_dir"
+mkdir -p "$groundingdino_dir" "$mobilesam_dir" "$labels_dir" "$clip_dir"
 
 wget -O "$groundingdino_dir/groundingdino_swint_ogc.pth" \
   "https://github.com/IDEA-Research/GroundingDINO/releases/download/v0.1.0-alpha/groundingdino_swint_ogc.pth"
@@ -24,8 +25,11 @@ wget -O "$mobilesam_dir/mobile_sam.pt" \
 wget -O "$labels_dir/HM3D_CountsOfObjectTypes.csv" \
   "https://raw.githubusercontent.com/crcz25/HOV-SG/main/hovsg/labels/HM3D_CountsOfObjectTypes.csv"
 
-wget -O "$models_dir/laion2b_s32b_b79k.bin" \
+wget -O "$clip_dir/H_laion2b_s32b_b79k.bin" \
   "https://huggingface.co/laion/CLIP-ViT-H-14-laion2B-s32B-b79K/resolve/main/open_clip_pytorch_model.bin?download=true"
+
+wget -O "$clip_dir/L_laion2b_s32b_b82k.bin" \
+  "  https://huggingface.co/laion/CLIP-ViT-L-14-laion2B-s32B-b82K/resolve/main/open_clip_pytorch_model.bin"
 
 cp "$groundingdino_dir/groundingdino_swint_ogc.pth" "$models_dir/groundingdino_swint_ogc.pth"
 cp "$groundingdino_dir/GroundingDINO_SwinT_OGC.py" "$models_dir/GroundingDINO_SwinT_OGC.py"
